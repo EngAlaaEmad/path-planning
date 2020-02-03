b660cc7fe8d3345a98401321fb862c36e959668f
adam-ubuntu <gy.adam90@gmail.com>
2020-02-03 15:08:56 -0800

    Put helper functions in helpers.cpp

--- src/planner.cpp
+++ src/planner.cpp
@@ -2,8 +2,11 @@
 #include "planner.h"
 #include "vehicle.h"
 
+Planner::Planner() {}
 
-vector<vector<double>> generate_trajectory(Vehicle car, double lane, vector<double> previous_path_x, vector<double> previous_path_y, vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y){
+Planner::~Planner() {}
+
+vector<vector<double>> Planner::generate_trajectory(Vehicle car, double lane, vector<double> previous_path_x, vector<double> previous_path_y, vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y){
   
   vector<double> anchor_x;
   vector<double> anchor_y;
--- src/planner.h
+++ src/planner.h
@@ -6,8 +6,9 @@
 class Planner {
 
     public:
-        vector<vector<double>> generate_trajectory(Vehicle car, double lane, vector<double> previous_path_x, vector<double> previous_path_y, vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y);
-
+    Planner();
+    vector<vector<double>> generate_trajectory(Vehicle car, double lane, vector<double> previous_path_x, vector<double> previous_path_y, vector<double> map_waypoints_s, vector<double> map_waypoints_x, vector<double> map_waypoints_y);
+    ~Planner();
 
 
 };
